9d25451b73da4eac07bcde32865a44c8d4ed719e
Cleazyy <cleazy2002@gmail.com>
2023-01-14 15:10:38 +0100

    add check if map is closed

--- srcs/map_check.c
+++ srcs/map_check.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 13:36:57 by fluchten          #+#    #+#             */
-/*   Updated: 2023/01/14 14:23:25 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/01/14 15:03:29 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,29 @@ static int	rectangular_map(char **map)
 	return (1);
 }
 
-static int	valid_items(char **map)
+static int	closed_map(char **map)
+{
+	int	x;
+	int y;
+
+	x = 0;
+	while (x < ft_strlen(map[0]))
+	{
+		if (map[0][x] != '1' || map[get_map_size(map) - 1][x] != '1')
+			return (0);
+		x++;
+	}
+	y = 0;
+	while (y < get_map_size(map))
+	{
+		if (map[y][0] != '1' || map[y][ft_strlen(map[0]) - 1] != '1')
+			return (0);
+		y++;
+	}
+	return (1);
+}
+
+/* static int	valid_items(char **map)
 {
 	int	x;
 	int	y;
@@ -45,12 +67,14 @@ static int	valid_items(char **map)
 		y++;
 	}
 	return (1);
-}
+} */
 
 void	check_valid_map(char **map)
 {
 	if (!rectangular_map(map))
 		exit_free_error("ERROR: Map is not rectangular!\n", map);
-	if (!valid_items(map))
-		exit_free_error("ERROR: Map contains invalid items!\n", map);
+	if (!closed_map(map))
+		exit_free_error("ERROR: Map is not closed by walls!\n", map);
+	// if (!valid_items(map))
+	// 	exit_free_error("ERROR: Map contains invalid items!\n", map);
 }
