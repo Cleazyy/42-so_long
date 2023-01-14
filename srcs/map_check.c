3a31de1d502b7833992709cbcade1d0fa7e1986e
Cleazyy <cleazy2002@gmail.com>
2023-01-14 13:46:24 +0100

    remove debug print

--- srcs/map_check.c
+++ srcs/map_check.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 13:36:57 by fluchten          #+#    #+#             */
-/*   Updated: 2023/01/14 13:43:41 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/01/14 13:46:12 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ static int	rectangular_map(char **map)
 	int y;
 
 	y = 0;
-	ft_printf("map size = %d\n", get_map_size(map));
 	while (y < get_map_size(map))
 	{
 		if (ft_strlen(map[y]) != ft_strlen(map[0]))
