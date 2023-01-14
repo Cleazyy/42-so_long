dc2a8ec916a7a1f60828f42336392df4fd6e30b7
Cleazyy <cleazy2002@gmail.com>
2023-01-14 14:26:45 +0100

    add function to check items

--- srcs/map_check.c
+++ srcs/map_check.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 13:36:57 by fluchten          #+#    #+#             */
-/*   Updated: 2023/01/14 14:11:14 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/01/14 14:23:25 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,31 @@ static int	rectangular_map(char **map)
 	int	y;
 
 	y = 0;
-	while (y < get_map_size(map))
+	while (map[y])
 	{
 		if (ft_strlen(map[y]) != ft_strlen(map[0]))
 			return (0);
+		y++;
+	}
+	return (1);
+}
+
+static int	valid_items(char **map)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			if (map[y][x] != '0' || map[y][x] != '1' || map[y][x] != 'C'
+				|| map[y][x] != 'E' || map[y][x] != 'P')
+				return (0);
+			x++;
+		}
 		y++;
 	}
 	return (1);
@@ -30,4 +51,6 @@ void	check_valid_map(char **map)
 {
 	if (!rectangular_map(map))
 		exit_free_error("ERROR: Map is not rectangular!\n", map);
+	if (!valid_items(map))
+		exit_free_error("ERROR: Map contains invalid items!\n", map);
 }
